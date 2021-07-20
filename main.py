99eeee14023880069fd8af262bfdd831b96fa8e6
unknown <danielalcarazmartinez41@gmail.com>
2021-07-19 23:32:43 -0700

    Se implementaron rutas en archivo main

--- main.py
+++ main.py
@@ -12,17 +12,17 @@ import bottle
 import routes.auth
 import routes.storage
 import models.base
-import routes.dell-warranty
+import routes.dell_warranty
 
 app = bottle.Bottle()
 
 app.mount("/auth", routes.auth.app)
 app.mount("/storage", routes.storage.app)
-app.mount("/dell/store, routes.dell/store.app")
-app.mount("/dell/info, routes.dell/info.app")
-app.mount("/dell/store/<code>, routes.dell/store/<code>.app")
-app.mount("/dell/info/list, routes.dell/info/list.app")
-app.mount("/dell/void, routes.dell/void.app")
+app.mount("/store", routes.dell_warranty.app)
+app.mount("/dell/info", routes.dell_warranty.app)
+app.mount("/dell/store/<code>", routes.dell_warranty.app)
+app.mount("/dell/info/list", routes.dell_warranty.app)
+app.mount("/dell/void", routes.dell_warranty.app)
 
 
 
