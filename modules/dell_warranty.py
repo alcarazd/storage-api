b8ef66194e331a65664848e62d751d0fcad4d8f0
Daniel Alcaraz Martinez <danielalcarazmartinez41@gmail.com>
2021-08-11 14:03:41 -0700

    Se agregaro la funcion para extraer informacion con st

--- modules/dell_warranty.py
+++ modules/dell_warranty.py
@@ -42,3 +42,29 @@ def get_device_list(devices=None, st=None):
 
 def get_device_out_warranty(*args, **kwargs):
     pass
+
+def get_warr_from_dell(svctag):
+    res = requests.get(URL.format(svctag))
+
+    if res.status_code != 200:
+        sys.stderr.write('[%s] Caught %i as the response code.\n' % (svctag, res.status_code))
+        sys.stderr.write('[%s] Unable to get details for given service tag.\n'
+                % svctag)
+        return False
+
+    fault = res.json['GetAssetWarrantyResponse']['GetAssetWarrantyResult']['Faults']
+    if fault is not None:
+        sys.stderr.write("[%s] Failed to find details. Sure it's a valid TAG?\n" % svctag )
+        return False
+
+    asset = res.json['GetAssetWarrantyResponse']['GetAssetWarrantyResult']['Response']['DellAsset']
+    model = asset['MachineDescription']
+    ent = asset['Warranties']['Warranty']
+    shipped = asset['ShipDate']
+
+    print 'Service Tag:        ', svctag
+    print ' Model:             ', model
+    print ' Shipped:           ', shipped, '\n'
+    print '{0:<20} {1:>15}'.format(*('Warranty Ends','ServiceLevelDescription'))
+    for warr in [(d['EndDate'],d['ServiceLevelDescription']) for d in ent]:
+        print '{0:<20} {1:>15}'.format(*warr)
